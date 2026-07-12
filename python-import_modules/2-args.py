35f0c697b44665b780c14d8c48e69f36a54e1fb2
lamisDiyaa <lamiiis.diyaa@gmail.com.com>
2026-07-12 12:01:45 -0700

    Add 2-args.py to print command line arguments

---
+++ python-import_modules/2-args.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    import sys
+    argv = sys.argv[1:]
+    count = len(argv)
+    
+    if count == 0:
+        print("0 arguments.")
+    elif count == 1:
+        print("1 argument:")
+    else:
+        print("{} arguments:".format(count))
+        
+    for i, arg in enumerate(argv, start=1):
+        print("{}: {}".format(i, arg))
