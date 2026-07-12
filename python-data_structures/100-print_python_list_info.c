07d3939acaf238e9ccdbafc3f1423a3dd901c412
lamisDiyaa <lamiiis.diyaa@gmail.com.com>
2026-07-12 13:40:28 -0700

    Add 100-print_python_list_info.c CPython source file

---
+++ python-data_structures/100-print_python_list_info.c
@@ -0,0 +1,26 @@
+#include <Python.h>
+#include <object.h>
+#include <listobject.h>
+#include <stdio.h>
+
+/**
+ * print_python_list_info - Prints basic info about Python lists
+ * @p: A PyObject representing a Python list
+ */
+void print_python_list_info(PyObject *p)
+{
+    Py_ssize_t size, allocated, i;
+    PyObject *item;
+
+    size = PyList_Size(p);
+    allocated = ((PyListObject *)p)->allocated;
+
+    printf("[*] Size of the Python List = %zd\n", size);
+    printf("[*] Allocated = %zd\n", allocated);
+
+    for (i = 0; i < size; i++)
+    {
+        item = PyList_GetItem(p, i);
+        printf("Element %zd: %s\n", i, Py_TYPE(item)->tp_name);
+    }
+}
