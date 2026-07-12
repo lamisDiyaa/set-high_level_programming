8f3818087b1bc2dce528a132c8a2537514b5fb0f
lamisDiyaa <lamiiis.diyaa@gmail.com.com>
2026-07-12 13:04:48 -0700

    Clean commit for 0-print_list_integer.py using heredoc

--- python-data_structures/0-print_list_integer.py
+++ python-data_structures/0-print_list_integer.py
@@ -1,7 +1,4 @@
 #!/usr/bin/python3
-
-
 def print_list_integer(my_list=[]):
-    """Prints all integers of a list."""
-    for num in my_list:
-        print("{}".format(num))
+    for i in my_list:
+        print("{:d}".format(i))
