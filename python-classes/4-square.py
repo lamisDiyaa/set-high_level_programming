aa884bfa3fd2b4d88bb7a511d9b0d9a4885a9857
lamisDiyaa <lamiiis.diyaa@gmail.com.com>
2026-07-18 04:24:08 -0700

    Complete task 4 with getter and setter properties

---
+++ python-classes/4-square.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""This module defines a class Square."""
+
+
+class Square:
+    """A class that defines a square."""
+    def __init__(self, size=0):
+        """Initializes the square."""
+        self.size = size
+
+    @property
+    def size(self):
+        """Retrieves the size of the square."""
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """Sets the size of the square with validation."""
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    def area(self):
+        """Returns the current square area."""
+        return self.__size * self.__size
