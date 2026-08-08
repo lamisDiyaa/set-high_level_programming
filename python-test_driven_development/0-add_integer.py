aa1d5a2cda93d4aae511bdacc2dfcef2a5999109
lamisDiyaa <lamiiis.diyaa@gmail.com.com>
2026-08-08 07:42:32 -0700

    Fix 0-add_integer.py, add README, and remove pycache

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -4,8 +4,8 @@
 
 def add_integer(a, b=98):
     """Adds 2 integers after casting floats to ints."""
-    if not isinstance(a, (int, float)):
+    if type(a) not in (int, float):
         raise TypeError("a must be an integer")
-    if not isinstance(b, (int, float)):
+    if type(b) not in (int, float):
         raise TypeError("b must be an integer")
     return int(a) + int(b)
