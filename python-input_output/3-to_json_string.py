0113d9c2e3f81b3074effc104c308c38402642ee
lamisDiyaa <lamiiis.diyaa@gmail.com.com>
2026-08-08 07:07:41 -0700

    Add 3-to_json_string.py

---
+++ python-input_output/3-to_json_string.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""Module that contains a function that returns JSON representation of an object."""
+import json
+
+
+def to_json_string(my_obj):
+    """Returns the JSON representation of an object (string)."""
+    return json.dumps(my_obj)
