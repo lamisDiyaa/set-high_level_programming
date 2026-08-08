b837f3867a0663dad7f3588631941ee3f5f8da92
Lamis Diyaa <lamiiis.diyaa@gmail.com>
2026-08-08 09:29:14 -0700

    Add Base class in models/base.py

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -1,11 +1,9 @@
 #!/usr/bin/python3
 """Module that contains the Base class."""
-import json
 
 
 class Base:
-    """Base class for all other classes in this project."""
-
+    """Base class for managing id attributes across the project."""
     __nb_objects = 0
 
     def __init__(self, id=None):
@@ -15,17 +13,3 @@ class Base:
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
-
-    @staticmethod
-    def to_json_string(list_dictionaries):
-        """Return the JSON string representation of list_dictionaries."""
-        if list_dictionaries is None or len(list_dictionaries) == 0:
-            return "[]"
-        return json.dumps(list_dictionaries)
-
-    @staticmethod
-    def from_json_string(json_string):
-        """Return the list represented by a JSON string."""
-        if json_string is None or len(json_string) == 0:
-            return []
-        return json.loads(json_string)
