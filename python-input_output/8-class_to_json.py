3962edc155daba258ac7dd7c3dba042c5db494c6
lamisDiyaa <lamiiis.diyaa@gmail.com.com>
2026-08-08 07:29:00 -0700

    Fix pycodestyle line length in 8-class_to_json.py

--- python-input_output/8-class_to_json.py
+++ python-input_output/8-class_to_json.py
@@ -1,9 +1,7 @@
 #!/usr/bin/python3
-"""Module that contains a function that returns the dictionary
-description with simple data structure for JSON serialization of an object.
-"""
+"""Module that returns the dictionary description for JSON serialization."""
 
 
 def class_to_json(obj):
-    """Returns the dictionary description for JSON serialization of an object."""
+    """Returns the dictionary description of an object."""
     return obj.__dict__
