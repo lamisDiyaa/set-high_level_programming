5ef32057e1a426f2abcf983c34e8fcb9158a933e
lamisDiyaa <lamiiis.diyaa@gmail.com.com>
2026-08-08 07:36:54 -0700

    Add 100-append_after.py

---
+++ python-input_output/100-append_after.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""Module that contains a function that inserts a line of text to a file
+after each line containing a specific string.
+"""
+
+
+def append_after(filename="", search_string="", new_string=""):
+    """Inserts a line of text to a file after each line containing string."""
+    text = ""
+    with open(filename, "r", encoding="utf-8") as f:
+        for line in f:
+            text += line
+            if search_string in line:
+                text += new_string
+
+    with open(filename, "w", encoding="utf-8") as f:
+        f.write(text)
