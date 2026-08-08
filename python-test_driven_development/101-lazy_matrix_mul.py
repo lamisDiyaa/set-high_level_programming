854074ce57a7c45c0145cfbb6b769e49d0d8ec93
lamisDiyaa <lamiiis.diyaa@gmail.com.com>
2026-08-08 08:05:18 -0700

    Use numpy matmul directly without custom validation in 101-lazy_matrix_mul.py

--- python-test_driven_development/101-lazy_matrix_mul.py
+++ python-test_driven_development/101-lazy_matrix_mul.py
@@ -5,37 +5,4 @@ import numpy as np
 
 def lazy_matrix_mul(m_a, m_b):
     """Multiplies 2 matrices by using NumPy."""
-    if not isinstance(m_a, list):
-        raise TypeError("m_a must be a list")
-    if not isinstance(m_b, list):
-        raise TypeError("m_b must be a list")
-
-    if not all(isinstance(row, list) for row in m_a):
-        raise TypeError("m_a must be a list of lists")
-    if not all(isinstance(row, list) for row in m_b):
-        raise TypeError("m_b must be a list of lists")
-
-    if m_a == [] or m_a == [[]]:
-        raise ValueError("m_a can't be empty")
-    if m_b == [] or m_b == [[]]:
-        raise ValueError("m_b can't be empty")
-
-    for row in m_a:
-        for elem in row:
-            if type(elem) not in (int, float):
-                raise TypeError("m_a should contain only integers or floats")
-
-    for row in m_b:
-        for elem in row:
-            if type(elem) not in (int, float):
-                raise TypeError("m_b should contain only integers or floats")
-
-    row_len_a = len(m_a[0])
-    if not all(len(row) == row_len_a for row in m_a):
-        raise TypeError("each row of m_a must be of the same size")
-
-    row_len_b = len(m_b[0])
-    if not all(len(row) == row_len_b for row in m_b):
-        raise TypeError("each row of m_b must be of the same size")
-
     return np.matmul(m_a, m_b)
