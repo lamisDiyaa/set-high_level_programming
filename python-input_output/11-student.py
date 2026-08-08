dad3a3621d3338fc1a00158cab16743dc3cee9f8
lamisDiyaa <lamiiis.diyaa@gmail.com.com>
2026-08-08 07:33:36 -0700

    Add 11-student.py

---
+++ python-input_output/11-student.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""Module that defines a Student class with reload capability."""
+
+
+class Student:
+    """Represent a student."""
+
+    def __init__(self, first_name, last_name, age):
+        """Initialize a new Student."""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """Get a dictionary representation of the Student."""
+        if isinstance(attrs, list) and all(isinstance(x, str) for x in attrs):
+            return {k: v for k, v in self.__dict__.items() if k in attrs}
+        return self.__dict__
+
+    def reload_from_json(self, json):
+        """Replace all attributes of the Student instance."""
+        for key, value in json.items():
+            setattr(self, key, value)
