c2437e5c13189571795aa2c7a2e3a1782e64ca9d
lamisDiyaa <lamiiis.diyaa@gmail.com>
2026-08-22 04:27:21 -0700

    Safe filter states from SQL injection

---
+++ python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""
+Displays all values in the states table of hbtn_0e_0_usa
+where name matches the argument, safe from MySQL injections.
+"""
+import MySQLdb
+import sys
+
+if __name__ == "__main__":
+    db = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=sys.argv[1],
+        passwd=sys.argv[2],
+        db=sys.argv[3]
+    )
+
+    cur = db.cursor()
+    query = (
+        "SELECT * FROM states "
+        "WHERE BINARY name = %s "
+        "ORDER BY id ASC"
+    )
+    cur.execute(query, (sys.argv[4],))
+
+    rows = cur.fetchall()
+    for row in rows:
+        print(row)
+
+    cur.close()
+    db.close()
