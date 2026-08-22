b75f33ce2cf0fcf9e0d719c117291a08f9082b4c
lamisDiyaa <lamiiis.diyaa@gmail.com>
2026-08-22 05:39:57 -0700

    Add City model and fetch cities by state

---
+++ python-object_relational_mapping/model_city.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+Defines the City class inheriting from Base
+"""
+from model_state import Base
+from sqlalchemy import Column, ForeignKey, Integer, String
+
+
+class City(Base):
+    """
+    City class links to the MySQL table 'cities'
+    """
+    __tablename__ = 'cities'
+
+    id = Column(Integer, primary_key=True, nullable=False, autoincrement=True)
+    name = Column(String(128), nullable=False)
+    state_id = Column(Integer, ForeignKey('states.id'), nullable=False)
