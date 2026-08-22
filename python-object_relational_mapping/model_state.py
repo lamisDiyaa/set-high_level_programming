1112c29b29e10fcfe8a60794a799fcf54f399a97
lamisDiyaa <lamiiis.diyaa@gmail.com>
2026-08-22 04:51:50 -0700

    Define State class using SQLAlchemy

---
+++ python-object_relational_mapping/model_state.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""
+Defines the State class and an instance Base = declarative_base()
+"""
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
+
+
+class State(Base):
+    """
+    State class inherits from Base and links to MySQL table 'states'
+    """
+    __tablename__ = 'states'
+
+    id = Column(Integer, primary_key=True, nullable=False, autoincrement=True)
+    name = Column(String(128), nullable=False)
