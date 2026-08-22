6d189eacdaf61b74ac85930c8b900439b533bf86
lamisDiyaa <lamiiis.diyaa@gmail.com>
2026-08-22 10:02:07 -0700

    Display X-Request-Id header using requests

---
+++ python-network_1/5-hbtn_header.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Sends a request to a URL and displays the value of X-Request-Id header"""
+import requests
+import sys
+
+
+if __name__ == "__main__":
+    url = sys.argv[1]
+    r = requests.get(url)
+    print(r.headers.get("X-Request-Id"))
