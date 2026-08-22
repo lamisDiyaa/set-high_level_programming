ba42f910eac3387b725793cac6009dc09b237f74
lamisDiyaa <lamiiis.diyaa@gmail.com>
2026-08-22 08:19:41 -0700

    Send POST request with email parameter

---
+++ python-network_1/2-post_email.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""
+Sends a POST request to a passed URL with an email parameter,
+and displays the body of the response decoded in utf-8.
+"""
+import sys
+import urllib.parse
+import urllib.request
+
+
+if __name__ == "__main__":
+    url = sys.argv[1]
+    values = {'email': sys.argv[2]}
+    data = urllib.parse.urlencode(values).encode('ascii')
+
+    req = urllib.request.Request(url, data)
+    with urllib.request.urlopen(req) as response:
+        print(response.read().decode('utf-8'))
